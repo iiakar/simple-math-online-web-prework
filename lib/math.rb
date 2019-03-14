060e5f4a5b09d1ed6548610e8880d351e429e20d
iiakar <ibsakar@gmail.com>
2019-03-14 19:39:17 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,19 @@
-def addition(num1, num2)
+def addition(5 + 4)
 end
 
-def subtraction(num1, num2)
+def subtraction(10 - 5)
 end
 
-def division(num1, num2)
+def division(50 / 2)
 end
 
-def multiplication(num1, num2)
+def multiplication(4 ** 30)
 end
 
-def modulo(num1, num2)
+def modulo(34 % 5)
 end
 
-def square_root(num)
+def math.sqrt (81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
